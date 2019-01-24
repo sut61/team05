7af96fbe740ddddc73f170de1c2305163af74c8d
prewphayom <prewphayom.191@gmail.com>
2019-01-25 01:24:03 +0700

    add controller -close #29

---
+++ frontend/src/app/controller/controller.ts
@@ -0,0 +1,39 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+@Injectable()
+
+export class Controller {
+public API = '//localhost:8080';
+
+constructor(
+            private httpClient:HttpClient
+    ){}
+
+      getsender(): Observable<any> {
+        return this.httpClient.get(this.API + '/Sender');
+      }
+
+      getreceiver(): Observable<any> {
+        return this.httpClient.get(this.API + '/Receiver');
+      }
+
+      getlink(): Observable<any> {
+        return this.httpClient.get(this.API + '/Link');
+      }
+
+      getprovince(): Observable<any> {
+      return this.httpClient.get(this.API + '/Province');
+      }
+
+      getprovincesen(): Observable<any> {
+      return this.httpClient.get(this.API + '/Provincesen');
+      }
+
+      getgender(): Observable<any> {
+      return this.httpClient.get(this.API + '/Gender');
+      }
+
+
+}
