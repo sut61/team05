55d019ffe04d5177aaf580b6373ee744c4feff0e
kannika-kan <kannika.kan1996@hotmail.com>
2019-01-25 00:03:17 +0700

    edit entity

--- backend/src/main/java/com/sut/se/g05/DemoApplication.java
+++ backend/src/main/java/com/sut/se/g05/DemoApplication.java
@@ -1,14 +1,69 @@
 package com.okta.developer.demo;
 
+import java.util.stream.Stream;
+
+import com.sut.se.g05.entity.*;
+import com.sut.se.g05.repository.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.BeanCreationException;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.stereotype.Component;
+import org.springframework.boot.ApplicationArguments;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.Stream;
+
+@CrossOrigin(origins = "http://localhost:4200")
 
 @SpringBootApplication
 public class DemoApplication {
+	@Autowired private	BankempRepository bankempRepository;
+	@Autowired private	GenderRepository genderRepository;
+	@Autowired private	InformationempRepository informationempRepository;
+	@Autowired private	PositionRepository positionRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-}
 
+	@Component
+	class DataSetup implements ApplicationRunner{
+		@Override
+		public void run(ApplicationArguments args) throws NullPointerException{
+
+
+			Bankemp bankemp1 = new Bankemp("Thai Bank");
+			bankempRepository.save(bankemp1);
+			Bankemp bankemp2 = new Bankemp("K Bank");
+			bankempRepository.save(bankemp2);
+
+
+			Gender gender1 = new Gender("Male");
+			genderRepository.save(gender1);
+			Gender gender2 = new Gender("Female");
+			genderRepository.save(gender2);
+
+
+			Position position1 = new Position("Driver");
+			positionRepository.save(position1);
+			Position position2 = new Position("Manager");
+			positionRepository.save(position2);
+			Position position3 = new Position("Secretar");
+			positionRepository.save(position3);
+			Position position4 = new Position("HR");
+			positionRepository.save(position4);
+
+
+			bankempRepository.findAll().forEach(System.out::println);
+			genderRepository.findAll().forEach(System.out::println);
+			positionRepository.findAll().forEach(System.out::println);
+
+		}
+	}
+}
---
+++ backend/src/main/java/com/sut/se/g05/repository/InformationempRepository.java
@@ -0,0 +1,11 @@
+package com.sut.se.g05.repository;
+
+import com.example.demo.entity.Informationemp;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+@RepositoryRestResource
+@CrossOrigin(origins = "http://localhost:4200")
+public interface InformationempRepository extends JpaRepository<Informationemp, Long> {
+    Informationemp findByinformationempid (String informatioempid);
+}
