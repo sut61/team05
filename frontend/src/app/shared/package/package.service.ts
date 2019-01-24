6204b359480f0bfa903ccd96f80839dbb7cdadea
wsnoom213 <weerawat213@gmail.com>
2019-01-25 04:17:30 +0700

    add shared

---
+++ frontend/src/app/shared/package/package.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { Employee } from '../../employee';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PackageService {
+  public API = '//localhost:8080';
+
+  private employee = new  BehaviorSubject(new Employee())
+  currentEmployee = this.employee.asObservable()
+
+  constructor(private http: HttpClient) {}
+
+  getSender(): Observable<any> {
+    return this.http.get(this.API + '/sender');
+  }
+
+  getReceiver(): Observable<any> {
+    return this.http.get(this.API + '/receiver');
+  }
+
+  getProvince(): Observable<any> {
+    return this.http.get(this.API + '/province');
+  }
+
+  getEmployee(): Observable<any> {
+    return this.http.get(this.API + '/employee');
+  }
+
+  login(user: string,password : string): Observable<any> {
+    return this.http.get(this.API + '/Login/'+user+'/'+password);
+  }
+
+  setEmployee(data: Employee) {
+    this.employee.next(data);
+  }
+}
+
