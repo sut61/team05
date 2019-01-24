272642d98d32543e61299eb5f5c6360ec849baaa
kannika-kan <kannika.kan1996@hotmail.com>
2019-01-24 23:22:53 +0700

    add entity

---
+++ backend/src/main/java/com/sut/se/g05/entity/Informationemp.java
@@ -0,0 +1,49 @@
+package com.okta.developer.demo;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name="Informationemp")
+public class Informationemp {
+
+    @Id
+    @SequenceGenerator(name = "info_seq", sequenceName = "info_seq")
+    @GeneratedValue(generator = "info_seq", strategy = GenerationType.SEQUENCE)
+    private Long informationempid;
+    String firstname;
+    String lastname;
+    String phone;
+    String address;
+    String banknumber;
+    String email;
+    String password;
+
+    @ManyToOne
+    private Bankemp bankemp;
+    @ManyToOne
+    private Gender gender;
+    @ManyToOne
+    private Position position;
+
+    public Informationemp(){}
+
+    public Informationemp(String firstname,String lastname,String phone,String address,String banknumber,String email,
+                          String password,Bankemp bankemp,Gender gender,Position position){
+        this.firstname=firstname;
+        this.lastname=lastname;
+        this.phone = phone;
+        this.address=address;
+        this.banknumber = banknumber;
+        this.email = email;
+        this.password = password;
+        this.bankemp=bankemp;
+        this.gender=gender;
+        this.position = position;
+
+
+    }
+
+}
