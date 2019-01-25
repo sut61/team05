2647447eccff3dc5f2f38952bec7db931b347dde
wsnoom213 <weerawat213@gmail.com>
2019-01-25 19:45:38 +0700

    add packageUI - close #21

---
+++ frontend/src/app/package/package.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import { PackageService } from '../shared/package/package.service';
+import { Router } from '@angular/router';
+import { MatDialogRef } from '@angular/material';
+import { HttpClient} from '@angular/common/http';
+import { Employee } from '../employee';
+
+@Component({
+  selector: 'app-package',
+  templateUrl: './package.component.html',
+  styleUrls: ['./package.component.css']
+})
+export class PackageComponent implements OnInit {
+
+senders: Array<any>;
+receivers: Array<any>;
+provinces: Array<any>;
+
+employee = new Employee();
+supply = '';
+senderSelect = '';
+receiverSelect = '';
+provinceSelect = '';
+
+  constructor(private httpClient: HttpClient, private router: Router, private PackageService: PackageService) {
+    PackageService.currentEmployee.subscribe(data =>{
+        this.employee = data
+      })
+   }
+
+  ngOnInit() {
+    this.PackageService.getSender().subscribe(data => {
+      this.senders = data;
+      console.log(this.senders);
+    });
+
+    this.PackageService.getReceiver().subscribe(data => {
+      this.receivers = data;
+      console.log(this.receivers);
+    });
+
+    this.PackageService.getProvince().subscribe(data => {
+      this.provinces = data;
+      console.log(this.provinces);
+    });
+
+  }
+  cancel() {
+    this.router.navigate(['/app-menu']);
+  }
+
+  save() {
+    if (this.senderSelect === '' && this.supply === '' && this.receiverSelect === '' && this.provinceSelect === '') {
+      alert('กรุณากรอกข้อมูลให้ครบถ้วน');
+    } else {
+      this.httpClient.post('http://localhost:8080/package/' + this.senderSelect + '/' + this.supply +
+                            '/' +this.receiverSelect +'/' + this.provinceSelect + '/' + this.employee.employeeId, {})
+      .subscribe(
+          data => {
+              alert("บันทึกสำเร็จ");
+              this.router.navigate(['/app-menu']);
+          }
+
+      );
+    }
+  }
+}
