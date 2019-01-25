ec932042315008b3c61df813afd482e61f265b4b
shompoosks <wipaweesukkasemshompoo@gmail.com>
2019-01-25 18:21:26 +0700

    add UI Carry -close #46

---
+++ frontend/src/app/carry/carry.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Controller } from '../controller/controller.service';
+import { Router } from "@angular/router";
+
+@Component({
+  selector: 'app-carry',
+  templateUrl: './carry.component.html',
+  styleUrls: ['./carry.component.css']
+})
+export class CarryComponent implements OnInit {
+  packageId : Array<any>
+  status : Array<any>
+  receivers : Array<any>
+  nameprovince : Array<any>
+  
+  select: any = {
+    packageId : '',
+    date : '',
+    carryNumber : '',
+    status : '',
+    firstname : '',
+    lastname : '',
+    Address : '',
+    Telephone : '',
+    nameprovince : ''
+    
+   
+  };
+ 
+  constructor(private router:Router, private controllerService: Controller,private httpClient: HttpClient) { }
+
+  ngOnInit() {
+    this.controllerService.getPackages().subscribe(data => {
+      this.packageId = data;
+      console.log(this.packageId);
+    });
+    this.controllerService.getProvince().subscribe(data => {
+      this.nameprovince = data;
+      console.log(this.nameprovince);
+    });
+    this.controllerService.getReceiver().subscribe(data => {
+      this.receivers = data;
+      console.log(this.receivers);
+    });
+  }
+
+  save() {
+      this.httpClient.post('http://localhost:8080/carrys/' + this.select.packageId + '/' + this.select.date + '/' 
+      + this.select.carryNumber + '/' + this.select.status + '/' + this.select.firstname + '/' + this.select.nameprovince ,this.select)
+      .subscribe(
+          data => {
+              console.log('PUT Request is successful', data);
+          },
+          error => {
+              console.log('Error to PUT Request', error);
+          }
+      );
+    }
+}
+
+
