ce3844794f6f52d8a4dd9c4cc9acf9990fec8190
prewphayom <prewphayom.191@gmail.com>
2019-02-09 17:11:20 +0700

    add and update repository -close #78

---
+++ backend/src/main/java/com/sut/se/g05/repository/LinkRepository.java
@@ -0,0 +1,12 @@
+package com.sut.se.g05.repository;
+
+import com.sut.se.g05.entity.Link;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@RepositoryRestController
+@CrossOrigin (origins = "http: //localhost:4200")
+public interface LinkRepository extends JpaRepository<Link, Long> {
+    Link findBylinkId (Long linkId);
+}
