00644b5958a14dbda2cc85ece7da7b1edd4d1d16
kannika-kan <kannika.kan1996@hotmail.com>
2019-02-09 16:46:04 +0700

    add repository -close #78

--- backend/src/main/java/com/sut/se/g05/repository/LinksenRepository.java
+++ backend/src/main/java/com/sut/se/g05/repository/LinksenRepository.java
@@ -1,12 +1,12 @@
 package com.sut.se.g05.repository;
 
-import com.sut.se.g05.entity.Sender;
+import com.sut.se.g05.entity.Linksen;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestController
 @CrossOrigin (origins = "http: //localhost:4200")
-public interface SenderRepository extends JpaRepository<Sender, Long> {
-    Sender findByfirstname (String firstname);
+public interface LinksenRepository extends JpaRepository<Linksen, Long> {
+    Linksen findBylinksenId (Long linksenId);
 }
