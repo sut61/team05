5df9b26305866a4f9eabd8b165e9021a297962f7
prewphayom <43243793+prewphayom@users.noreply.github.com>
2019-03-05 22:37:30 +0700

    Update LinksenRepository.java

--- backend/src/main/java/com/sut/se/g05/repository/LinksenRepository.java
+++ backend/src/main/java/com/sut/se/g05/repository/LinksenRepository.java
@@ -8,5 +8,5 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @RepositoryRestController
 @CrossOrigin (origins = "http: //localhost:4200")
 public interface LinksenRepository extends JpaRepository<Linksen, Long> {
-    Linksen findBylinksenId (Long linksenId);
+    Linksen findBylinksen (String linksen);
 }
