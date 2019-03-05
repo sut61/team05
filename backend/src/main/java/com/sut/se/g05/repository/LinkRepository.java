d6f05bf2cb651708239c92e5a4f217400b97835a
prewphayom <43243793+prewphayom@users.noreply.github.com>
2019-03-05 22:37:57 +0700

    Update LinkRepository.java

--- backend/src/main/java/com/sut/se/g05/repository/LinkRepository.java
+++ backend/src/main/java/com/sut/se/g05/repository/LinkRepository.java
@@ -8,5 +8,5 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @RepositoryRestController
 @CrossOrigin (origins = "http: //localhost:4200")
 public interface LinkRepository extends JpaRepository<Link, Long> {
-    Link findBylinkId (Long linkId);
+    Link findBylink (String link);
 }
